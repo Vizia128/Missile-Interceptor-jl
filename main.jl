8ddc4504b8a44f94b5704a9f86fa4d5256f7b2ff
Julian Kizanis <julianzia@gmail.com>
2022-09-12 02:43:40 -0600

    Added pose

--- main.jl
+++ main.jl
@@ -1,7 +1,6 @@
-using Agents, Parameters, Grassmann
+using Agents, Parameters, Makie, Grassmann
 
-basis"0,1,1,1"  # Initialize to PGA
-
+basis"0,1,1,1"  # Initialize GA to PGA
 
 space = ContinuousSpace((1.0, 1.0, 1.0); periodic = true)
 
@@ -12,11 +11,12 @@ space = ContinuousSpace((1.0, 1.0, 1.0); periodic = true)
     fuel::Float64
     alive::Bool = true
 
-    pose = 0.0 + 0.0v₁ + 0.0v₂ + 0.0v₃ + 0.0v₄ + 0.0v₁₂₃ + 0.0v₁₂₄ + 0.0v₁₃₄ + 0.0v₂₃₄ + 0.0v₁₂₃₄ # motor
-    rate = 0.0v₁₂ + 0.0v₁₃ + 0.0v₁₄ + 0.0v₂₃ + 0.0v₂₄ + 0.0v₃₄  # line
-    inertia = 0.0v₁₂ + 0.0v₁₃ + 0.0v₁₄ + 0.0v₂₃ + 0.0v₂₄ + 0.0v₃₄   # line
+    pose::MultiVector
+    rate::MultiVector
+    inertia::MultiVector
 end
 
 Rocket(id, pos, energy) = Projectile(id, pos, :rocket, fuel)
 Missile(id, pos, energy) = Projectile(id, pos, :missile, fuel)
 Interceptor(id, pos, energy) = Projectile(id, pos, :interceptor, fuel)
+
